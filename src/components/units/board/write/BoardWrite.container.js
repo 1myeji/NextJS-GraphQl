594879fa8c05ee82e70d20d3980e5d66e00ed5f9
1myeji <106291546+1myeji@users.noreply.github.com>
2023-03-03 16:40:23 +0900

    Day6. 실무용 폴더구조와 props

---
+++ src/components/units/board/write/BoardWrite.container.js
@@ -0,0 +1,57 @@
+import BoardWriteUI from './BoardWrite.presenter';
+import { gql, useMutation } from '@apollo/client';
+import { useState } from 'react';
+
+const CREATE_BOARD = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    # 변수의 타입 적는 곳
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      # 실제 우리가 전달할 변수 적는 곳
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function BoardWrite() {
+  // 자바스크립트 영역
+  const [writer, setWriter] = useState('');
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
+  const [나의함수] = useMutation(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    // const writer = 'qqq' // 이 함수에 있으면 현재 스코프
+    const result = await 나의함수({
+      variables: {
+        // variables 이게 $ 역할을 해줌
+        writer: writer, // 이 함수에 없으면 스코프 체인을 통해서 위 함수에서 찾음
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    alert(result.data.createBoard.message);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  //HTML 영역(return 아래)
+  return (
+    <BoardWriteUI
+      aaa={onClickSubmit}
+      bbb={onChangeWriter}
+      ccc={onChangeTitle}
+      ddd={onChangeContents}
+    />
+  );
+}
