55080acd7a9e08844c72e54598de99a1526253ba
1myeji <106291546+1myeji@users.noreply.github.com>
2023-03-11 16:25:37 +0900

    Day13. 레이아웃 구조잡기 및 props.children에 대한 이해

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 // import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import type { AppProps } from "next/app";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Layout from "../src/components/commons/layout";
 
 export default function App({ Component }: AppProps) {
   const client = new ApolloClient({
-    uri: 'http://practice.codebootcamp.co.kr/graphql',
+    uri: "http://practice.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(), // 나중에 하기
   });
 
   return (
     <ApolloProvider client={client}>
-      <Component />
+      <Layout>
+        <Component />
+      </Layout>
     </ApolloProvider>
   );
 }
