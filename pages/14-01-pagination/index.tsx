4a915076bbfd61666f42f99d355cb7f216303b4b
1myeji <106291546+1myeji@users.noreply.github.com>
2023-03-14 10:13:31 +0900

    Day15. 무한스크롤

--- pages/14-01-pagination/index.tsx
+++ pages/14-01-pagination/index.tsx
@@ -13,15 +13,16 @@ const FETCH_BOARDS = gql`
   }
 `;
 export default function StaticRoutedPage() {
-  const { data, refetch } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(FETCH_BOARDS); 
+  const { data, refetch } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(FETCH_BOARDS);
 
   const onClickPage = (event: MouseEvent<HTMLSpanElement>) => {
-    void refetch({page: Number(event.currentTarget.id) })
-  }
+    void refetch({ page: Number(event.currentTarget.id) });
+  };
 
   return (
     <>
-      {data?.fetchBoards.map(el => {  // el에 빨간줄이 뜰텐데 fetchBoards에 type을 지정해주면 됩니다.
+      {data?.fetchBoards.map(el => {
+        // el에 빨간줄이 뜰텐데 fetchBoards에 type을 지정해주면 됩니다.
         return (
           <div key={el._id}>
             <span style={{ margin: "10px" }}>{el.writer}</span>
@@ -30,9 +31,16 @@ export default function StaticRoutedPage() {
         );
       })}
 
-      {new Array(10).fill(1).map((_, index) => ( // _ data 값을 안 쓴다는 의미
-        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>{index + 1}</span> // id는 숫자가 못들어감
-      ))}
+      {new Array(10).fill(1).map(
+        (
+          _,
+          index, // _ data 값을 안 쓴다는 의미
+        ) => (
+          <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
+            {index + 1}
+          </span> // id는 숫자가 못들어감
+        ),
+      )}
 
       {/* {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => ( // _ data 값을 안 쓴다는 의미
         <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>{index + 1}</span> // id는 숫자가 못들어감
