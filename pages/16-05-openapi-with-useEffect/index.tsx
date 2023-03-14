cc472cfc86ba13c313162108a06d0b70361f7c3d
1myeji <106291546+1myeji@users.noreply.github.com>
2023-03-14 18:12:39 +0900

    Day16. openAPI practice

--- pages/16-05-openapi-with-useEffect/index.tsx
+++ pages/16-05-openapi-with-useEffect/index.tsx
@@ -3,18 +3,26 @@ import { useEffect, useState } from "react";
 
 export default function OpenApiWithUseEffectPage() {
   const [dogUrl, setDogUrl] = useState("");
+  const [bible, setBible] = useState("");
 
   useEffect(() => {
     const fetchDog = async () => {
       const result = await axios.get("https://dog.ceo/api/breeds/image/random");
       setDogUrl(result.data.message);
     };
+    const fetchBible = async () => {
+      const result = await axios.get("https://bible-api.com/john 3:16");
+      console.log(result);
+      setBible(result.data.text);
+    };
     void fetchDog();
+    void fetchBible();
   }, []);
 
   return (
     <>
       <img src={dogUrl} alt="" />
+      <p>{bible}</p>
     </>
   );
 }
